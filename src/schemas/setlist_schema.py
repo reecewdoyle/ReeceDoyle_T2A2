35eaad4b7e7e03fcd28b5a18a61eb90702f195c5
reecewdoyle <reece.doyle@icloud.com>
2024-03-20 00:59:57 +1100

    added song  Model, Schema and CLI Command, tweaked setlist

--- src/schemas/setlist_schema.py
+++ src/schemas/setlist_schema.py
@@ -2,7 +2,7 @@ from init import ma
 
 class SetlistSchema(ma.Schema):
     class Meta:
-        fields = ("id", "song_id", "song_name", "song_type")
+        fields = ("id", "song_id", "song_title", "song_type")
 
 setlist_schema = SetlistSchema()
 setlists_schema = SetlistSchema(many=True)
