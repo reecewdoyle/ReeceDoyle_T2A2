80dc750fa8f950b6ab1116e379e430c8782a0035
reecewdoyle <reece.doyle@icloud.com>
2024-03-19 21:30:14 +1100

    added Venue model, schema and Cli commands

---
+++ src/schemas/venue_schema.py
@@ -0,0 +1,8 @@
+from init import ma 
+
+class VenueSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "manager", "address", "phone")
+
+venue_schema = VenueSchema()
+venues_schema = VenueSchema(many=True)
