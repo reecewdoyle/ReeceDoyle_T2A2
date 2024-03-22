af86d8a4221ef2987b179f0828a1acc75efd6db0
reecewdoyle <reece.doyle@icloud.com>
2024-03-23 01:45:15 +1100

    added jwt to aisle route

--- src/controllers/aisle_controller.py
+++ src/controllers/aisle_controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
 from init import db
 from models.aisle import AisleSong 
 from schemas.aisle_schema import aisle_songs_schema, aisle_song_schema
@@ -20,3 +22,19 @@ def get_one_aisle_song(aisle_song_id):
         return aisle_song_schema.dump(aisle_song)
     else:
         return {"error": f"Aisle song with id {aisle_song_id} not found"}, 404
+    
+@aisle_bp.route("/", methods=["POST"])
+@jwt_required()
+def create_aisle_song():
+    body_data = request.get_json()
+    aisle_song = AisleSong(
+        title=body_data.get("title"),
+        artist=body_data.get("artist"),
+        genre=body_data.get("genre"),
+        key=body_data.get("key"),
+        tempo=body_data.get("tempo"),
+        user_id = get_jwt_identity()
+    )
+    db.session.add(aisle_song)
+    db.session.commit()
+    return aisle_song_schema.dump(aisle_song)
