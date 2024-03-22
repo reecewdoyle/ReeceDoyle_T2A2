4a4a316af40d53b18b34e16ece88393233df51f8
reecewdoyle <reece.doyle@icloud.com>
2024-03-23 01:53:29 +1100

    added jwt to first_dance route

--- src/controllers/first_dance_controller.py
+++ src/controllers/first_dance_controller.py
@@ -1,7 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
 from init import db
 from models.first_dance import FirstDanceSong
 from schemas.first_dance_schema import first_dance_songs_schema, first_dance_song_schema
+
 
 first_dance_bp = Blueprint("firstdance", __name__, url_prefix="/firstdance")
 
@@ -19,3 +22,19 @@ def get_first_dance_song(first_dance_song_id):
         return first_dance_song_schema.dump(first_dance)
     else:
         return {"error": f"First Dance Song with id {first_dance_song_id} not found"}, 404
+    
+@first_dance_bp.route("/", methods=["POST"])
+@jwt_required()
+def creats_first_dance_song():
+    body_data = request.get_json()
+    first_dance_song = FirstDanceSong(
+        title=body_data.get("title"),
+        artist=body_data.get("artist"),
+        genre=body_data.get("genre"),
+        key=body_data.get("key"),
+        tempo=body_data.get("tempo"),
+        user_id = get_jwt_identity()
+    )
+    db.session.add(first_dance_song)
+    db.session.commit()
+    return first_dance_song_schema.dump(first_dance_song)
