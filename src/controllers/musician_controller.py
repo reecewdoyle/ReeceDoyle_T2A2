13e800914418b7f7ccd59f3309008546014f6b8d
reecewdoyle <reece.doyle@icloud.com>
2024-03-23 16:26:46 +1100

    added update route to musician

--- src/controllers/musician_controller.py
+++ src/controllers/musician_controller.py
@@ -48,3 +48,19 @@ def delete_musician(musician_id):
         return {"message": f"Musician {musician.name} deleted successfully"}
     else:
         return {"message": f"Musician {musician_id} was not found"}, 404
+    
+@musician_bp.route("/<int:musician_id>", methods=["PUT", "PATCH"])
+def update_musician(musician_id):
+    body_data = request.get_json()
+    stmt = db.select(Musician).filter_by(id=musician_id)
+    musician = db.session.scalar(stmt)
+    if musician:
+        musician.name = body_data.get("name") or musician.name
+        musician.email = body_data.get("email") or musician.email
+        musician.phone = body_data.get("phone") or musician.phone
+        musician.instrument = body_data.get("instrument") or musician.instrument
+
+        db.session.commit()
+        return musician_schema.dump(musician)
+    else:
+        return {"error": f"Musician with id {musician_id} not found"}, 404
