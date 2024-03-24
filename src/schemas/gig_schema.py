10ef45eccab29681cf943f6faf166fab2c0b3ca5
reecewdoyle <reece.doyle@icloud.com>
2024-03-24 12:26:28 +1100

    messing with the gig schema to try and fix the json output in insomnia

--- src/schemas/gig_schema.py
+++ src/schemas/gig_schema.py
@@ -2,6 +2,13 @@ from init import ma
 from marshmallow import fields
 
 class GigSchema(ma.Schema):
+    # agent_id= fields.Nested("AgentSchema", only=["name", "phone", "email"])
+    # user_id= fields.Nested("UserSchema", only=["name", "email"])
+    # musician_id = fields.Nested("MusicianSchema", only=["name", "instrument"])
+    # venue_id = fields.Nested("VenueSchema", only=["title", "address"])
+    # aisle_song_id = fields.Nested("AisleSongSchema", only=["title", "key", "tempo"])
+    # first_dance_song_id = fields.Nested("FirstDanceSongSchema", only=["title", "key", "tempo"])
+
     agent = fields.Nested("AgentSchema", only=["name", "phone", "email"])
     user = fields.Nested("UserSchema", only=["name", "email"])
     musician = fields.Nested("MusicianSchema", only=["name", "instrument"])
@@ -10,9 +17,13 @@ class GigSchema(ma.Schema):
     first_dance_song = fields.Nested("FirstDanceSongSchema", only=["title", "key", "tempo"])
 
     class Meta:
+        # fields = ("id", "date", "time", "invoice", "venue_id", "agent_id", "user_id", "musician_id", "aisle_song_id", "first_dance_song_id")
         fields = ("id", "date", "time", "invoice", "venue", "agent", "user", "musician", "aisle_song", "first_dance_song")
         ordered = True
 
 gig_schema = GigSchema()
 gigs_schema = GigSchema(many=True)
+
+
+
 
